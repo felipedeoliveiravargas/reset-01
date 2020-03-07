220e88a3a97bb8f110a72d5df69b36a0fb31cc00
Felipe de Oliveira Vargas <felipe.vargas@altus.com.br>
2020-03-07 16:27:38 -0300

    Criação de classes e alteração do Programa

--- src/App.java
+++ src/App.java
@@ -1,10 +1,35 @@
+import sun.rmi.runtime.NewThreadAction;
+
+import java.util.concurrent.Callable;
+
 public class App {
 
     public static void main(String []args){
+        int valorUm = 33;
+        int valorDois = 45;
         System.out.println("Hello World");
-        System.out.println("Hello World\noiii");
-        System.out.println("Hoje é Sábado");
-        System.out.println("Dia de estudar");
 
-    }
-}
+        Calculadora calculadoraDeIdades = new Calculadora();
+        int somaDasIdades = calculadoraDeIdades.somar(valorUm, valorDois);
+        System.out.println("Soma das Idades: " + somaDasIdades);
+
+        int subtracaoDasIdades = calculadoraDeIdades.subtrai(valorUm, valorDois);
+        System.out.println("Subtração das Idades: " + subtracaoDasIdades);
+
+        int multiplicacaoDasIdades = calculadoraDeIdades.multiplica(valorUm, valorDois);
+        System.out.println("Multiplicação das Idades: " + multiplicacaoDasIdades);
+
+        double num1 = new Double(valorUm);
+        double num2 = new Double(valorDois);
+        double divisaoDasIdades = calculadoraDeIdades.divide(num1, num2);
+        System.out.println("Divisão das Idades: " + divisaoDasIdades);
+
+        Comparador comparadorDeIdades = new Comparador();
+         boolean comparacaoDeIdades = comparadorDeIdades.menorQue (valorUm,valorDois);
+        System.out.println("Comparação de Idades: " + comparacaoDeIdades);
+
+
+
+                    }
+
+        };
---
+++ src/Comparador.java
@@ -0,0 +1,6 @@
+public class Comparador {
+    boolean menorQue (int valorUm, int valorDois){
+        return valorUm < valorDois ;
+    }
+
+}
