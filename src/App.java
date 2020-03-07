9ea993b80b3172b7783e43ea5e7f8fdc356fcf05
Felipe de Oliveira Vargas <felipe.vargas@altus.com.br>
2020-03-07 12:06:57 -0300

    Alterei o programa

--- src/App.java
+++ src/App.java
@@ -3,8 +3,8 @@ public class App {
     public static void main(String []args){
         System.out.println("Hello World");
         System.out.println("Hello World\noiii");
-        System.out.println("Sem criatividade");
-        System.out.println("Para mensagens");
+        System.out.println("Hoje é Sábado");
+        System.out.println("Dia de estudar");
 
     }
 }
