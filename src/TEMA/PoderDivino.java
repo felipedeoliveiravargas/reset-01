38a61198df63cf98276d97d3b6a3243006f88094
Felipe de Oliveira Vargas <felipe.vargas@altus.com.br>
2020-03-09 17:24:10 -0300

    Criação da classe Poder Divino

--- src/TEMA/PoderDivino.java
+++ src/TEMA/PoderDivino.java
@@ -1,4 +1,12 @@
 package TEMA;
 
 public class PoderDivino {
+    string nome;
+    double intensidadeDeFe;
+    double custoDeFe;
+
+    public PoderDivino (string nome, double intensidadeDeFe, double custoDeFe) {
+        this.nome = nome;
+        this.intensidadeDeFe = intensidadeDeFe;
+        this.custoDeFe = custoDeFe ;
 }
